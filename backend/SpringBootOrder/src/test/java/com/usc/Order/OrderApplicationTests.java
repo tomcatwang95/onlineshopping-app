459c450b2b85f94af1583c723a721920a24daccb
QFang111 <116602556+QFang111@users.noreply.github.com>
2022-10-25 12:01:37 -0400

    first commit

---
+++ backend/SpringBootOrder/src/test/java/com/usc/Order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usc.Order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
